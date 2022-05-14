abe6a413782c8019586078e710be4678661636ba
vyacheslav <vyacheslav.shejkin@gmail.com>
2022-05-14 15:58:53 +0300

    Запускаю скролл и драг только после загрузки лоттитолько после

--- js/script.js
+++ js/script.js
@@ -47,72 +47,68 @@ if (window.screen.width > 479) {
     var scroller_4 = { wheelMultiplier: getLineHeight(), ease: 0.055, speed: 0, y: 0 };
 }
 
-if (window.screen.width < 480) {
-    let touch_up = 0;
-    let touch_down = 0;
-    let touch_path = 0;
-    let touch_speed = 0.05;
-    let touch_inertia = 0;
-    // let touch_inertia_lerp = 0;
-    let point_click = false;
-    let first_click = false;
-    window.addEventListener('touchstart', point_down);
-    window.addEventListener('touchend', point_cancel);
-    window.addEventListener('touchmove', pointer_move);
-    function pointer_move(e) {
-        touch_up = e.touches[0].clientY;
-        if (point_click == true) {
-            if (first_click == true) {
-                touch_path = touch_up - touch_down;
-                let touch_inertia = 0.07*Math.pow(Math.abs(touch_path), 1.05);
-                if (touch_inertia > 0.5) {
-                    scroller_1.speed -= touch_path * scroller_1.wheelMultiplier * touch_speed * touch_inertia;
-                    scroller_2.speed -= touch_path * scroller_2.wheelMultiplier * touch_speed * touch_inertia;
-                    scroller_3.speed -= touch_path * scroller_3.wheelMultiplier * touch_speed * touch_inertia;
-                    scroller_4.speed -= touch_path * scroller_4.wheelMultiplier * touch_speed * touch_inertia;
-                } else {
-                    scroller_1.speed -= touch_path * scroller_1.wheelMultiplier * touch_speed;
-                    scroller_2.speed -= touch_path * scroller_2.wheelMultiplier * touch_speed;
-                    scroller_3.speed -= touch_path * scroller_3.wheelMultiplier * touch_speed;
-                    scroller_4.speed -= touch_path * scroller_4.wheelMultiplier * touch_speed;
-                }
-            };
-            first_click = true;
+let touch_up = 0;
+let touch_down = 0;
+let touch_path = 0;
+let touch_speed = 0.05;
+let touch_inertia = 0;
+// let touch_inertia_lerp = 0;
+let point_click = false;
+let first_click = false;
+
+function pointer_move(e) {
+    touch_up = e.touches[0].clientY;
+    if (point_click == true) {
+        if (first_click == true) {
+            touch_path = touch_up - touch_down;
+            let touch_inertia = 0.07*Math.pow(Math.abs(touch_path), 1.05);
+            if (touch_inertia > 0.5) {
+                scroller_1.speed -= touch_path * scroller_1.wheelMultiplier * touch_speed * touch_inertia;
+                scroller_2.speed -= touch_path * scroller_2.wheelMultiplier * touch_speed * touch_inertia;
+                scroller_3.speed -= touch_path * scroller_3.wheelMultiplier * touch_speed * touch_inertia;
+                scroller_4.speed -= touch_path * scroller_4.wheelMultiplier * touch_speed * touch_inertia;
+            } else {
+                scroller_1.speed -= touch_path * scroller_1.wheelMultiplier * touch_speed;
+                scroller_2.speed -= touch_path * scroller_2.wheelMultiplier * touch_speed;
+                scroller_3.speed -= touch_path * scroller_3.wheelMultiplier * touch_speed;
+                scroller_4.speed -= touch_path * scroller_4.wheelMultiplier * touch_speed;
+            }
         };
-        touch_down = e.touches[0].clientY;
+        first_click = true;
     };
-    function point_down() {
-        if (Math.abs(scroller_1.speed) > 1) {
-            scroller_1.speed = 0;
-            scroller_2.speed = 0;
-            scroller_3.speed = 0;
-            scroller_4.speed = 0;
-        }
-        point_click = true;
-    };
-    function point_cancel() {
-        point_click = false;
-        first_click = false;
-    };
-} else {
-    window.addEventListener('wheel', onWheel);
-    function onWheel(event) {
+    touch_down = e.touches[0].clientY;
+};
+function point_down() {
+    if (Math.abs(scroller_1.speed) > 1) {
+        scroller_1.speed = 0;
+        scroller_2.speed = 0;
+        scroller_3.speed = 0;
+        scroller_4.speed = 0;
+    }
+    point_click = true;
+};
+function point_cancel() {
+    point_click = false;
+    first_click = false;
+};
+
+function onWheel(event) {
 // отключил потому что не знаю что строчка делает и консоль много ругается
 //        event.preventDefault();
-        var normalized;
-        var delta = event.wheelDelta;
-        if (delta) {
-            delta = event.deltaY || event.detail || 0;
-            normalized = delta % 120 == 0 ? delta / 120 : delta / 120;
-        } else {
-            normalized = delta % 120 == 0 ? delta / 120 : delta / 120;
-        }
-        scroller_1.speed += normalized * scroller_1.wheelMultiplier;
-        scroller_2.speed += normalized * scroller_2.wheelMultiplier;
-        scroller_3.speed += normalized * scroller_3.wheelMultiplier;
-        scroller_4.speed += normalized * scroller_4.wheelMultiplier;
+    var normalized;
+    var delta = event.wheelDelta;
+    if (delta) {
+        delta = event.deltaY || event.detail || 0;
+        normalized = delta % 120 == 0 ? delta / 120 : delta / 120;
+    } else {
+        normalized = delta % 120 == 0 ? delta / 120 : delta / 120;
     }
-};
+    scroller_1.speed += normalized * scroller_1.wheelMultiplier;
+    scroller_2.speed += normalized * scroller_2.wheelMultiplier;
+    scroller_3.speed += normalized * scroller_3.wheelMultiplier;
+    scroller_4.speed += normalized * scroller_4.wheelMultiplier;
+}
+
 function getLineHeight() {
     var element = document.createElement("div");
     element.style["font-size"] = "128ex";
@@ -230,6 +226,11 @@ function lottie_load(){
         setTimeout(() => {
             redis_img.style.transform = "translate(0vh) rotate(0deg)";
         }, 700);
+
+        window.addEventListener('touchstart', point_down);
+        window.addEventListener('touchend', point_cancel);
+        window.addEventListener('touchmove', pointer_move);
+        window.addEventListener('wheel', onWheel);
 
     }
 }
