6c224a5c165239802daef20c6ea2694d21541422
nickozy <104327959+nickozy@users.noreply.github.com>
2022-05-16 14:53:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let path_loop_once = true;
 gsap.registerPlugin(MotionPathPlugin);
 var path_tween = gsap.to(".lottie-wrapper", {
 //    ease: "power1.inOut",
-    ease: linear,
+    ease: "linear",
     motionPath: { path: "#path", align: "#path", alignOrigin: [0.5, 0.5] }
 });
 
