f999c4b88498b78735fb42829db09141125e600a
nickozy <104327959+nickozy@users.noreply.github.com>
2022-05-16 16:13:32 +0300

    Тестирую транформ на кнопку вместо sroller_1.y

--- js/script.js
+++ js/script.js
@@ -170,7 +170,8 @@ function onFrame() {
 bt_anchor_1 = document.querySelector(".bt-anchor-1");
 bt_anchor_2 = document.querySelector(".bt-anchor-2");
 bt_anchor_1.addEventListener('click', function (event) {
-    scroller_1.y = 2876;
+    target_1.style.transform = translate3d(0px, -1142.05px, 0px);
+    // scroller_1.y = 2876;
     scroller_2.y = 2876;
     scroller_3.y = 2876;
     scroller_4.y = 2876;
