d51538c85388bf49d98b6c478b7ed2783f2bfbe8
noftr <97229373+noftr@users.noreply.github.com>
2022-05-18 16:18:25 +0300

    снова поменял число у редиски

--- js/script.js
+++ js/script.js
@@ -253,7 +253,7 @@ bt_anchor_2.addEventListener('click', function (event) {
         scroller_1.y = innerWidth * -1.345;
         scroller_2.y = innerWidth * -1.345;
         scroller_3.y = innerWidth * -1.345;
-        scroller_4.y = innerWidth * 100;
+        scroller_4.y = innerWidth * 99.4;
     } else {
         scroller_1.y = innerWidth * -3.3;
         scroller_2.y = innerWidth * -3.3;
